2d58ba68e8964bc34d958740e29e11a3420ab681
SamWang05 <wangsam00207@gmail.com>
2026-07-13 19:19:44 -0400

    updated skills and abilities UI to react to mouse clicks

--- js/skillsModule.js
+++ js/skillsModule.js
@@ -12,22 +12,31 @@ const selectColor = "#C9C9C9";
 function initializeSkillGroupListeners() {
     skillGroupsNodeArray.forEach((skillGroup) => {
         skillGroup.addEventListener("mouseenter", (event) => {
-            const targetNode = event.target;
+            const targetNode = event.currentTarget;
+            const targetClassList = targetNode.classList[1];
 
-            targetNode.style.backgroundColor = selectColor;
-            targetNode.style.marginRight = "0px";
-            targetNode.style.borderRadius = "25px 0px 0px 25px";
+            skillGroupsNodeArray.forEach((skill) => {
+                const skillClassList = skill.classList[1];
+
+                if (skillClassList == targetClassList) {
+                    skill.style.backgroundColor = selectColor;
+                    skill.style.marginRight = "0px";
+                    skill.style.borderRadius = "25px 0px 0px 25px";
+                }
+                else {
+                    skill.style.backgroundColor = baseColor;
+                    skill.style.marginRight = "50px";
+                    skill.style.borderRadius = "25px";
+
+                    skillGroupDisplayNode.style.backgroundColor = baseColor;
+                }
+            });
+
+            switch(targetClassList) {
+                
+            }
 
             skillGroupDisplayNode.style.backgroundColor = selectColor;
-        });
-        skillGroup.addEventListener("mouseleave", (event) => {
-            const targetNode = event.target;
-
-            targetNode.style.backgroundColor = baseColor;
-            targetNode.style.marginRight = "50px";
-            targetNode.style.borderRadius = "25px";
-
-            skillGroupDisplayNode.style.backgroundColor = baseColor;
         });
     });
 }
@@ -36,8 +45,17 @@ function animateSkillGroupSelectors(skillGroupNode) {
 
 }
 
+function defaultSkillGroupSelection() {
+    softwareSkillNode.style.backgroundColor = selectColor;
+    softwareSkillNode.style.marginRight = "0px";
+    softwareSkillNode.style.borderRadius = "25px 0px 0px 25px";
+
+    skillGroupDisplayNode.style.backgroundColor = selectColor;
+}
+
 function main() {
     initializeSkillGroupListeners();
+    defaultSkillGroupSelection();
 }
 
 main();
