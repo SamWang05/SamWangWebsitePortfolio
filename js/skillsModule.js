6343ebe9b0ac1ea0b125b695c0cd42b78c4f5f0c
SamWang05 <wangsam00207@gmail.com>
2026-07-13 18:33:19 -0400

    Added reactive UI tabs to indicate selection of skills

--- js/skillsModule.js
+++ js/skillsModule.js
@@ -6,17 +6,29 @@ const hardwareSkillNode = document.querySelector(".hardwareSkills");
 const labSkillNode = document.querySelector(".labSkills");
 const languageSkillNode = document.querySelector(".languageSkills");
 
-function initializeSkillGroupSelectors() {
+function initializeSkillGroupListeners() {
     skillGroupsNodeArray.forEach((skillGroup) => {
         skillGroup.addEventListener("mouseenter", (event) => {
             const targetNode = event.target;
-            console.log(targetNode);
+
+            targetNode.style.transform = "scale(1.25, 1.25)";
+            skillGroupDisplayNode.style.transform = "scale(1.1, 1.1)";
+        });
+        skillGroup.addEventListener("mouseleave", (event) => {
+            const targetNode = event.target;
+
+            targetNode.style.transform = "scale(1, 1)";
+            skillGroupDisplayNode.style.transform = "scale(1, 1)";
         });
     });
 }
 
+function animateSkillGroupSelectors(skillGroupNode) {
+
+}
+
 function main() {
-    initializeSkillGroupSelectors();
+    initializeSkillGroupListeners();
 }
 
 main();
