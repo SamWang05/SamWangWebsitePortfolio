8bfae862c2a426c14e2736b51f1dd62070f69b0d
SamWang05 <wangsam00207@gmail.com>
2026-07-13 18:47:01 -0400

    Reorganized skillsModules UI

--- js/skillsModule.js
+++ js/skillsModule.js
@@ -6,19 +6,28 @@ const hardwareSkillNode = document.querySelector(".hardwareSkills");
 const labSkillNode = document.querySelector(".labSkills");
 const languageSkillNode = document.querySelector(".languageSkills");
 
+const baseColor = "#D9D9D9";
+const selectColor = "#C9C9C9";
+
 function initializeSkillGroupListeners() {
     skillGroupsNodeArray.forEach((skillGroup) => {
         skillGroup.addEventListener("mouseenter", (event) => {
             const targetNode = event.target;
 
-            targetNode.style.transform = "scale(1.25, 1.25)";
-            skillGroupDisplayNode.style.transform = "scale(1.1, 1.1)";
+            targetNode.style.backgroundColor = selectColor;
+            targetNode.style.marginRight = "0px";
+            targetNode.style.borderRadius = "25px 0px 0px 25px";
+
+            skillGroupDisplayNode.style.backgroundColor = selectColor;
         });
         skillGroup.addEventListener("mouseleave", (event) => {
             const targetNode = event.target;
 
-            targetNode.style.transform = "scale(1, 1)";
-            skillGroupDisplayNode.style.transform = "scale(1, 1)";
+            targetNode.style.backgroundColor = baseColor;
+            targetNode.style.marginRight = "50px";
+            targetNode.style.borderRadius = "25px";
+
+            skillGroupDisplayNode.style.backgroundColor = baseColor;
         });
     });
 }
