6e3014dc3304f6b3faa647c0abd12f574867490c
SamWang05 <wangsam00207@gmail.com>
2026-07-17 00:21:14 -0400

    updated font size and formatting of experience intro display banner

--- js/cycleRotatingSubtitle.js
+++ js/cycleRotatingSubtitle.js
@@ -11,7 +11,7 @@ const observerOptions = {
 
 const subtitleTextArray = [
     "Education",
-    "Work Experience",
+    "Employment",
     "Extracurriculars",
     "Volunteering",
     "Mentorship"
