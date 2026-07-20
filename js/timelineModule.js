9b5cd4a9572ef0b5ccfe1032ebcdb06bc451cfeb
SamWang05 <wangsam00207@gmail.com>
2026-07-20 18:07:17 -0400

    timeline now shows what year and month is being displayed

--- js/timelineModule.js
+++ js/timelineModule.js
@@ -4,13 +4,21 @@ const timelineDisplay = document.querySelector(".timelineDisplay");
 let timelineCurrentLength = 300;
 const ONE_SCROLL_PX_INCREMENT = 100;
 
+const TIMELINE_START_DATE = Temporal.PlainDate.from("2023-09-01");
+
 function calculateTimelineLength() {
-    const TIMELINE_START_DATE = Temporal.PlainDate.from("2023-09-01");
     const TIMELINE_END_DATE = Temporal.Now.plainDateISO();
 
-    let monthsElapsed = (TIMELINE_END_DATE.month + 12 * TIMELINE_END_DATE.year) - (TIMELINE_START_DATE.month + 12 * TIMELINE_START_DATE.year) + 6; // Add 6 to give some headway;
+    let monthsElapsed = (TIMELINE_END_DATE.month + 12 * TIMELINE_END_DATE.year) - 
+    (TIMELINE_START_DATE.month + 12 * TIMELINE_START_DATE.year) + 6; // Add 6 months to give some headway;
 
     return monthsElapsed;
+}
+
+function calculateDateFromMonths(months){
+    let dateFromStart = TIMELINE_START_DATE.add({ months: months });
+
+    return (dateFromStart.year + "-" + dateFromStart.month);
 }
 
 function timelineScrollListener(sliderNode) {
@@ -39,6 +47,8 @@ function timelineScrollListener(sliderNode) {
             }
         )
 
+        sliderNode.textContent = calculateDateFromMonths(Math.floor(currentScrollPos / 100, 1));
+
         timelineCurrentLength = timelineCurrentLength + differentialScroll;
         
         lastScrollPos = currentScrollPos;
@@ -61,6 +71,8 @@ function renderTimeline(months) {
 
     const timelineSlider = document.createElement("div");
 
+        timelineSlider.classList.add("bodyText");
+
         timelineSlider.style.backgroundColor = "#8C7B6B";
 
         timelineSlider.style.width = "20px";
@@ -70,6 +82,16 @@ function renderTimeline(months) {
         timelineSlider.style.maxHeight = "90%";
 
         timelineSlider.style.borderRadius = "10px";
+
+        timelineSlider.style.paddingBottom = "10px";
+
+        timelineSlider.style.writingMode = "vertical-rl";
+        timelineSlider.style.textAlign = "right";
+
+        timelineSlider.style.fontSize = "1rem";
+        timelineSlider.style.color = "#DDD9D3";
+
+        timelineSlider.textContent = "2023-7";
 
         timelineSliderFrame.appendChild(timelineSlider);
 
