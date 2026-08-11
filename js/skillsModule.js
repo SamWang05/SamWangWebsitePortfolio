66b66d3c16c4d61667e87003bb2c9450da24cedd
SamWang05 <wangsam00207@gmail.com>
2026-08-11 15:58:12 -0400

    Updated skilsModule with Altium instead of KiCAD

--- js/skillsModule.js
+++ js/skillsModule.js
@@ -58,7 +58,7 @@ function renderSoftwareSkills () {
         ["Python", "82%", "Automation, Data Processing and Visualization", "Python.svg"],
         ["C/C++", "73%", "Object-Oriented Programming, Data Structures, Time/Space Optimization", "cLogo.svg"],
         ["MATLAB", "68%", "Matrix Operations, 3D Visualization, Signal Analysis", "MATLAB.svg"],
-        ["PCB Design Software", "31%", "Altium Designer, KiCAD, Circuit Construction, Simulation, Component Selection, PCB Design and Layout", "KiCAD.svg"],
+        ["PCB Design Software", "72%", "Altium Designer, Circuit Construction, Simulation, Component Selection, PCB Design and Layout", "Altium.png"],
         ["Circuit Simulation", "57%", "PSPICE/LTSPICE, Digital Logic, Transistor Circuits", "logicORGate.svg"],
         ["Dev Tools and IDEs", "69%", "VSCode, GitHub, Keil uVision, Arduino IDE, Chrome Dev Tools", "VSCode.svg"],
         ["Assembly", "23%", "Machine Code, Hardware Architecture", "Verilog.svg"],
